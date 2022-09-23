05c0ae2327f09f26e9e1f00eb58e1916ca592107
prelipceancristian <37293618+prelipceancristian@users.noreply.github.com>
2022-09-24 00:35:07 +0300

    second commit

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new DiscordJs.Client({
+const client : DiscordJs.Client = new DiscordJs.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
